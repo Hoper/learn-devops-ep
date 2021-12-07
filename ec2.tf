0d19de09874dc19262ea94e065897864d4492622
Hoper <dzianis_plastsinin@epam.com>
2021-12-07 17:39:36 +0300

    change ami to ubuntu 20.04

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,10 @@
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
 data "aws_ssm_parameter" "linuxAmi" {
   provider = aws.region-master
-  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+#  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+  name     = "/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
+
 
 #Get Linux AMI ID using SSM Parameter endpoint in us-west-2
 #data "aws_ssm_parameter" "linuxAmiOregon" {
